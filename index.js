452e0fd55893e8bec6362a9913fe0594238d3740
Anand <126659841+anandk3012@users.noreply.github.com>
2023-12-14 11:29:59 +0530

    Add files via upload
    VVMA Website Project Files

---
+++ index.js
@@ -0,0 +1,11 @@
+// var isScroll = false;
+// document.addEventListener("scroll",function(){
+//     $(".header").css("background-color","rgba(255,255,255,0.8)")
+//     isScroll = true;
+//     setTimeout(function(){
+//         if(isScroll){
+//             $(".header").css("background-color","rgb(255, 229, 164)")
+//             isScroll = false;
+//         }
+//     },600)
+// })
